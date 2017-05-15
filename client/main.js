e7c3b6575cfcb629e550f34538aed55a7dd182e6
Tomas <bratrahim@gmail.com>
2017-05-15 15:34:28 +0100

    Set up the user profile route
    Likes counter has to be done

--- client/main.js
+++ client/main.js
@@ -4,28 +4,38 @@ Meteor.subscribe('posts');
 
 Template.postFeed.helpers(
     {
-        posts:function(){return Posts.find({},{sort:{submitted:-1}});}
+        posts: function () {
+            return Posts.find({}, {sort: {submitted: -1}});
+        }
     });
 
+Template.post_card.helpers(
+    {
+        //query for counting likes
+        likes: function () {
+            return Posts.find({_id: this._id},{liked: {$exists:true}}).count();
+        }
+    }
+);
+
 Template.post_card.events({
-   "click .likeButton": function (parentContext) {
-       console.log(this);
-       Posts.update({_id: this._id }, { $inc : { likes: 1 } } );
-   }
+    "click .likeButton": function (parentContext) {
+        Posts.update({_id: this._id}, {$push: {liked: {userID: Meteor.userId()}}});
+    }
 });
 
 Template.postSubmit.events({
-   "submit form":function (e) {
-       e.preventDefault();
+    "submit form": function (e) {
+        e.preventDefault();
 
-       var post = {
-           img: $(e.target).find('[name=imageUrl]').val(),
-           description: $(e.target).find('[name=description]').val()
-       };
-       Meteor.call('postInsert', post, function (error,result) {
-           if(error)
-               return alert(error.reason);
-           Router.go('postpage',{_id:result._id});
-       })
-   }
+        var post = {
+            img: $(e.target).find('[name=imageUrl]').val(),
+            description: $(e.target).find('[name=description]').val()
+        };
+        Meteor.call('postInsert', post, function (error, result) {
+            if (error)
+                return alert(error.reason);
+            Router.go('postpage', {_id: result._id});
+        })
+    }
 });
---
+++ client/templates/userprofile.js
@@ -0,0 +1,6 @@
+/**
+ * Created by admin on 5/15/2017.
+ */
+Template.userprofile.rendered = function () {
+    $('ul.tabs').tabs();
+};
